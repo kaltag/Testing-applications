f44ba9109572758c0eb24440487cc106990834d4
kaltag <alexx30rus@gmail.com>
2023-04-12 22:56:07 +0400

    feat: add foreing keys

--- db/migrate/20230412075117_create_answers.rb
+++ db/migrate/20230412075117_create_answers.rb
@@ -3,6 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.string :body, null: false
       t.boolean :correct, default: false
+      t.references :question, null: false, foreign_key: true
 
       t.timestamps
     end
