63dd5d12da180174802ff8659d85102a4dc7772d
kaltag <alexx30rus@gmail.com>
2023-04-13 13:46:15 +0400

    fix: fix migration

--- db/migrate/20230412185224_add_categories_ref_to_tests.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoriesRefToTests < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :tests, :category, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_185224) do
+ActiveRecord::Schema.define(version: 2023_04_12_075728) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 2023_04_12_185224) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 1
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "category_id", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
