bef54f31b4c3d2b28726e1dbc6d74dcc0847d00b
kaltag <alexx30rus@gmail.com>
2023-04-13 23:17:15 +0400

    fix: fix method get_tests_with_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
-  # я не смог придумать как без ассоциаций и общей таблицы сохранять тесты которые проходил юзер. Так же я добавил в параметры самого юзера,
-  # тк у нас еще нет девайса и @current.user если смотреть для текущего пользователя
-  def method_name(user, level)
-    user.tests.where(level:)
+  def get_tests_with_level(level)
+    tests.where(level:)
   end
 end
