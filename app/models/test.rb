a56d6fb6793aa55e70757bed814eac7bb952ff2a
kaltag <44153751+kaltag@users.noreply.github.com>
2023-04-18 18:43:50 +0400

    Lesson 6 upate (#7)

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,16 +5,18 @@ class Test < ApplicationRecord
   has_many :users, through: :user_tests
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
-  scope :correct_answer, -> { where(correct: true) }
-
   scope :easy_tests, -> { where(level: 0..1) }
   scope :medium_tests, -> { where(level: 2..4) }
   scope :hard_tests, -> { where(level: 5..Float::INFINITY) }
 
-  scope :get_tests_name, ->(category_name) { Test.joins(:category).where(categories: { title: category_name }).order(title: :DESC).pluck('tests.title') }
+  scope :get_tests_category, ->(category_name) { Test.joins(:category).where(categories: { title: category_name }) }
 
   validates :title, presence: true
   validates :level, presence: true
   validates :title, uniqueness: { scope: :level }
   validates_numericality_of :level, greater_than_or_equal_to: 0
+
+  def self.get_tests_name(category_name)
+    Test.get_tests_category(category_name).order(title: :DESC).pluck('tests.title')
+  end
 end
