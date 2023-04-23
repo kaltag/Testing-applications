af1f1982bdbc50961fcac4c10dcccdddc3644729
kaltag <alexx30rus@gmail.com>
2023-04-23 19:39:24 +0400

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
-    include BetterHtml::Helpers
+  include BetterHtml::Helpers
+
+  def current_year
+    Time.zone.now.year
+  end
+
+  def github_ulr(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
