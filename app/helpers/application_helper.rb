f0941562ecf9433ba80c116865119ea16b9e76b0
kaltag <alexx30rus@gmail.com>
2023-04-29 14:10:00 +0400

    add navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
   def github_ulr(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message
+     if flash[:alert]
+       content_tag :p, flash[:alert], class: 'flash alert'
+     end
+  end
 end
