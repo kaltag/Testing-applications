ce8072b01cc7b10d0561c0b6f73b81a3076e0f3e
kaltag <alexx30rus@gmail.com>
2023-04-29 14:31:10 +0400

    session cockie current url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_to(cookies.delete(:intended_url) || tests_path)
     else
       flash.now[:alert] = 'Verify email or password'
       render :new
