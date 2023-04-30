b317a8901e606f71e7e8e548131a23ebeaf1d6ae
kaltag <alexx30rus@gmail.com>
2023-04-30 11:31:46 +0400

    add index to db users:email

--- db/migrate/20230427075719_add_email_password_digest_to_users.rb
+++ db/migrate/20230427075719_add_email_password_digest_to_users.rb
@@ -2,5 +2,6 @@ class AddEmailPasswordDigestToUsers < ActiveRecord::Migration[6.1]
   def change
     rename_column :users, :name, :email
     add_column :users, :password_digest, :string
+    add_index :users, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2023_04_27_075719) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
