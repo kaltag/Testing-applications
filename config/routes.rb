f43602f836b3433bb347f441f9cd5c7b6dd6558c
kaltag <alexx30rus@gmail.com>
2023-05-01 19:44:18 +0400

    admin tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root to: 'tests#index'
 
-  devise_for :users, path: :gurus,
-    path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :tests do
+  resources :tests, only: :index do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
+
     member do
       post :start
     end
@@ -16,6 +16,14 @@ Rails.application.routes.draw do
   resources :test_passages, only: %i[show update] do
     member do
       get :result
+    end
+  end
+
+  namespace :admin do
+    resources :tests do
+      # resources :questions, shallow: true, except: :index do
+      #   resources :answers, shallow: true, except: :index
+      # end
     end
   end
 
