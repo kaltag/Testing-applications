c5fdb14f18ceca4e4a142d6174c36a091a5f1e1d
kaltag <alexx30rus@gmail.com>
2023-05-02 12:24:18 +0400

    add first/last name for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   def after_sign_in_path_for(current_user)
@@ -6,6 +7,18 @@ class ApplicationController < ActionController::Base
       admin_tests_path
     else
       root_path
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:first_name, :last_name, :email, :password)
     end
   end
 end
