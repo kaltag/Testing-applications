25c541c4ca96b30ae0e9f6114f2fd2764bb6d8f8
kaltag <alexx30rus@gmail.com>
2023-06-06 15:30:23 +0400

    update badges services for category and level

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,5 @@
 class Badge < ApplicationRecord
-  enum rule: { category_complete: 0, first_attempt: 1, easy_tests: 2, medium_tests: 3, hard_tests: 4 }
+  enum rule: { category_ruby: 0, first_attempt: 1, easy_tests: 2, medium_tests: 3, hard_tests: 4 }
 
   has_many :user_rewards, dependent: :destroy
   has_many :users, through: :user_rewards
--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -3,6 +3,9 @@ class BadgeService
     @test_passage = test_passage
     @user = @test_passage.user
     @test = @test_passage.test
+    @easy_lvl = 0..1
+    @medium_lvl = 2..4
+    @hard_lvl = 5..Float::INFINITY
   end
 
   def give_badge
@@ -22,39 +25,31 @@ class BadgeService
   end
 
   def easy_tests
-    user_easy_tests_ids = user_tests_by_easy_level.pluck(:test_id).uniq.sort
-    all_easy_tests_id = Test.easy_tests.published.pluck(:id).sort
-    user_easy_tests_ids.present? && user_easy_tests_ids == all_easy_tests_id
+    user_easy_tests_ids = user_tests_by_level(@easy_lvl).order(:test_id).pluck(:test_id).uniq
+    all_easy_tests_id = Test.easy_tests.published.order(:id).pluck(:id)
+    user_easy_tests_ids.present? && user_easy_tests_ids == all_easy_tests_id unless @user.badges.where(rule: 'easy_tests').present?
   end
 
   def medium_tests
-    user_medium_tests_ids = user_tests_by_medium_level.pluck(:test_id).uniq.sort
-    all_medium_tests_id = Test.medium_tests.published.pluck(:id).sort
-    user_medium_tests_ids.present? && user_medium_tests_ids == all_medium_tests_id
+    user_medium_tests_ids = user_tests_by_level(@medium_lvl).order(:test_id).pluck(:test_id).uniq
+    all_medium_tests_id = Test.medium_tests.published.order(:id).pluck(:id)
+    user_medium_tests_ids.present? && user_medium_tests_ids == all_medium_tests_id unless @user.badges.where(rule: 'medium_tests').present?
   end
 
   def hard_tests
-    user_hard_tests_ids = user_tests_by_hard_level.pluck(:test_id).uniq.sort
-    all_hard_tests_id = Test.hard_tests.published.pluck(:id).sort
-    user_hard_tests_ids.present? && user_hard_tests_ids == all_hard_tests_id
+    user_hard_tests_ids = user_tests_by_level(@hard_lvl).order(:test_id).pluck(:test_id).uniq
+    all_hard_tests_id = Test.hard_tests.published.order(:id).pluck(:id)
+    user_hard_tests_ids.present? && user_hard_tests_ids == all_hard_tests_id unless @user.badges.where(rule: 'hard_tests').present?
   end
 
-  def category_complete
-    user_category_tests_ids = user_tests_by_category.pluck(:test_id).uniq.sort
-    all_category_tests_id = Test.get_tests_category(@test.category.title).where(published: true).pluck(:id).sort
+  def category_ruby
+    user_category_tests_ids = user_tests_by_category.order(:test_id).pluck(:test_id).uniq
+    all_category_tests_id = Test.get_tests_category('Ruby').where(published: true).order(:id).pluck(:id)
     user_category_tests_ids.present? && user_category_tests_ids == all_category_tests_id
   end
 
-  def user_tests_by_easy_level
-    TestPassage.includes(:test).where(user: @user).where(success: true).where(tests: { level: 0..1 })
-  end
-
-  def user_tests_by_medium_level
-    TestPassage.includes(:test).where(user: @user).where(success: true).where(tests: { level: 2..4 })
-  end
-
-  def user_tests_by_hard_level
-    TestPassage.includes(:test).where(user: @user).where(success: true).where(tests: { level: 5..Float::INFINITY })
+  def user_tests_by_level(lvl)
+    TestPassage.includes(:test).where(user: @user).where(success: true).where(tests: { level: lvl })
   end
 
   def user_tests_by_category
